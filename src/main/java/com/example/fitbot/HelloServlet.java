2d494d17dfc47e8b50659b7bc6a0c05108915ea9
YohanNayanajith <yohannayanajith40@gmail.com>
2021-09-21 14:49:07 +0530

    Initial_Commit

---
+++ src/main/java/com/example/fitbot/HelloServlet.java
@@ -0,0 +1,25 @@
+package com.example.fitbot;
+
+import java.io.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Fitbot";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+    public void destroy() {
+    }
+}
