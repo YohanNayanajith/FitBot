88cd72f117a103e2c7b446c42245501ed3ce869b
YohanNayanajith <yohannayanajith40@gmail.com>
2021-10-01 03:21:49 +0530

    backend_modifications_1

---
+++ src/main/webapp/home.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    // $('.class_name').click(function () {
+    //     $(this).load("http://localhost:8080/quickSalon_war_exploded/upperStaff/owner/sideNav.html");
+    // });
+    // $(this).load("http://localhost:8080/FitBot_war_exploded/Landing/Navbar/NavBar.html");
+    // console.log("2758");
+    // $("button").click(function (){
+    //     console.log("Yohan Nayanajith 2758");
+    //     $(this).load("http://localhost:8080/FitBot_war_exploded/Landing/test.html");
+    // });
+
+    console.log("2758");
+    $("#class_name").load("http://localhost:8080/FitBot_war_exploded/Landing/test.html");
+});
+
