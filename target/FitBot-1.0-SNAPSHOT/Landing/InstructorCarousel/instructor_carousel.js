6072521a5ce9f55c00c2292ec041c6739a4dabf4
YohanNayanajith <yohannayanajith40@gmail.com>
2021-10-11 18:24:47 +0530

    backend for physical member

--- target/FitBot-1.0-SNAPSHOT/Landing/InstructorCarousel/instructor_carousel.js
+++ target/FitBot-1.0-SNAPSHOT/Landing/InstructorCarousel/instructor_carousel.js
@@ -1,19 +1,21 @@
-var slideIndex = 0;
-showSlides();
+let ins_slideIndex = 0;
+ins_showSlides();
 
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("instructor_image_sider");
-  var dots = document.getElementsByClassName("dot");
+function ins_showSlides() {
+  let i;
+  const slides = document.getElementsByClassName("instructor_image_sider");
+  const dots = document.getElementsByClassName("dot");
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";  
   }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1;}
+  ins_slideIndex++;
+  if (ins_slideIndex > slides.length) {
+    ins_slideIndex = 1;
+  }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " instructor_active";
+  slides[ins_slideIndex-1].style.display = "block";
+  dots[ins_slideIndex-1].className += " instructor_active";
   setTimeout(showSlides, 4000); // Change image every 4 seconds
 }
--- target/FitBot-1.0-SNAPSHOT/Landing/Navbar/NavBar.js
+++ target/FitBot-1.0-SNAPSHOT/Landing/Navbar/NavBar.js
@@ -1,6 +1,6 @@
 //mobile view
 function toggle_navbar() {
-    var x = document.getElementById("nav_mobile");
+    const x = document.getElementById("nav_mobile");
     if (x.className === "topnav") {
       x.className += " responsive";
     } else {
@@ -8,23 +8,9 @@ function toggle_navbar() {
     }
 }
 
-//scroll function
-// window.onscroll = function() {scrollFunction()};
-//
-// function scrollFunction() {
-//     var navbar = document.getElementById("nav_mobile");
-//     var sticky = navbar.offsetTop;
-//     if (window.pageYOffset >= sticky) {
-//         navbar.classList.add("fixed")
-//     }else {
-//         navbar.classList.remove("fixed");
-//         // navbar.classList.add("fixed")
-//     }
-// }
-
 // login button
 document.getElementById("login").onclick = function() {
-  location.href = "../Login2/login_test.html";
+  location.href = "../Login2/login.html";
 };
 
 // $(document).ready(function () {
